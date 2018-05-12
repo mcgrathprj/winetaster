c03307b7d7e7c7a0da504ed1ad9e6d252447de4c
Peter McGrath <mcgrathprj@gmail.com>
2018-05-12 11:59:45 -0400

    heroku setup

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/winetaster';
+exports.DATABASE_URL = process.env.MONGODB_URI || 'mongodb://localhost/winetaster';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/winetaster';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
