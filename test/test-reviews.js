ee07c384a840b388c10eb4235cd527b1d4d56fcf
Peter McGrath <mcgrathprj@gmail.com>
2018-06-11 20:30:05 -0400

    some test cleanup

--- test/test-reviews.js
+++ test/test-reviews.js
@@ -4,20 +4,19 @@ const chai = require('chai');
 const chaiHTTP = require('chai-http');
 
 const { app, runServer, closeServer } = require('../server');
-const { Wine } = require('../wines');
+const { Review } = require('../wines');
 const { TEST_DATABASE_URL } = require('../config');
 
 const expect = chai.expect;
 
 chai.use(chaiHTTP);
 
-describe('/data/wine', function () {
+describe('/data/review', function () {
   const username = 'exampleUser';
-  const wine = 'exampleWine';
-  const year = 'exampleYear';
-  const varietal = 'exampleVarietal';
-  const country = 'exampleCountry';
-  const region = 'exampleRegion';
+  const wine_id = 'exampleWineId';
+  const rating = 'exampleRating';
+  const title = 'exampleTitle';
+  const text = 'exampleText';
 
   before(function () {
     return runServer(TEST_DATABASE_URL);
@@ -30,56 +29,52 @@ describe('/data/wine', function () {
   beforeEach(function () { });
 
   afterEach(function () {
-    return Wine.remove({});
+    return Review.remove({});
 
-  describe('data/wines', function() {
+  describe('data/reviews', function() {
     describe('GET', function() {
        it('Should return an empty array initially', function () {
-          return chai.request(router).get('/data/wines').then(res => {
+          return chai.request(router).get('/data/reviews').then(res => {
             expect(res).to.have.status(200);
             expect(res.body).to.be.an('array');
             expect(res.body).to.have.length(0);
           });
         });
-      it ('should return an array of wines', function() {
-        return Wine.create (
+      it ('should return an array of reviews', function() {
+        return Review.create (
           {
             username,
-            wine,
-            year,
-            varietal,
-            country,
-            region
+            wine_id,
+            rating,
+            title,
+            text
           },
           {
             username: usernameB,
-            wine: wineB,
-            year: yearB,
-            varietal: varietalB,
-            country: countryB,
-            region: regionB
+            wine_id: wine_idB,
+            rating: ratingB,
+            title: titleB,
+            text: textB          
           }
         )
-          .then(() => chai.request(router).get('/data/wines'))
+          .then(() => chai.request(router).get('/data/reviews'))
           .then(res => {
             expect(res).to.have.status(200);
             expect(res.body).to.be.an('array');
             expect(res.body).to.have.length(2);
             expect(res.body[0]).to.equal({
               username,
-              wine,
-              year,
-              varietal,
-              country,
-              region
+              wine_id,
+              rating,
+              title,
+              text
               });
             expect(res.body[1]).to.equal({
               username: usernameB,
-              wine: wineB,
-              year: yearB,
-              varietal: varietalB,
-              country: countryB,
-              region: regionB
+              wine_id: wine_idB,
+              rating: ratingB,
+              title: titleB,
+              text: textB
               });
             }); 
           })
