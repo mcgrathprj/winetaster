3a432c4cdccfbea92b3a9cff274f2a89a9214b9f
Peter McGrath <mcgrathprj@gmail.com>
2018-09-01 17:23:40 -0400

    clean up full wine entry function

--- public/app.js
+++ public/app.js
@@ -268,7 +268,6 @@ function browseWines(callback) {
 }
 
 function displayAllWines(data) {
-  console.log(data);
   $(".home-screen").hide();
   $(".js-list-of-wines").show();
   $(".js-list-of-wines").empty();
@@ -276,14 +275,6 @@ function displayAllWines(data) {
   $(".js-list-of-wines").append(`<p><a href="#" onclick="getWineByID(displayFullWinePage, '${data[i]._id}')">${data[i].year} ${data[i].name}</a></p>`);
   }
 }
-
-// function displayFullWinePageStub() {
-//   $(".js-list-of-wines").hide();
-//   $(".js-full-wine-page").show();
-//   $(".js-full-wine-page").html(
-//     `<h2>Full Wine Page Will Go Here</h2>`
-//     )
-// }
 
 function displayFullWinePage(thisWine) {
   console.log(thisWine._id);
